f3819c69a60addd3a75ecbd1154e4f8442010f9d
Antoine Meresse <ant.meresse@gmail.com>
2020-12-11 00:18:57 +0100

    chore: start timing

--- src/components/Quizz.js
+++ src/components/Quizz.js
@@ -5,36 +5,53 @@ import Reponse from './Reponse'
 function Quizz({questionnaire}) {
     
     const [reponseSelect , setReponseSelect] = useState(-1);
-    const [questionNumber, setQuestionNumber] = useState(0);
+    const [questionNumber, setQuestionNumber] = useState(-1);
     const [compteur, setCompteur] = useState(0);
+    const [timers, setTimers] = useState([]);
 
     function nextQuestion() {
         if (reponseSelect !== -1) {
-
             if(reponseSelect === questionnaire[questionNumber].correctAnswer) {
                 setCompteur(compteur+1);
             }
-
             setQuestionNumber(questionNumber+1);
             setReponseSelect(-1);
+            
+            addTimers()
         }
+    }
+
+    function startQuizz() {
+        console.log("Debut");
+        addTimers()
+        setQuestionNumber(0);
+    }
+
+    function addTimers(){
+        setTimers([...timers, new Date()])
+        console.log(timers);
     }
     
     return (
         <div className="quizz">
-            <h1>I'm a quizz component</h1>
             <p>Score : {compteur} / {questionnaire.length}</p>
-            { (questionNumber < questionnaire.length) && (
-                <>    
-                <p>Reponse Selectionnee : {reponseSelect} | {questionNumber}/{questionnaire.length}</p>
-                <Question question={questionnaire[questionNumber].question}/>
-                {
-                    questionnaire[questionNumber].choices.map(
-                        (elem, index) => <Reponse value={elem} onReponseSelect={setReponseSelect} index={index} key={index} selected={reponseSelect}/>
-                    )
-                }
-                <button onClick={() => nextQuestion()}>Next</button>
-                </>
+            { questionNumber < 0 ? 
+            (
+                <button onClick={() => startQuizz()}>Start</button>
+            ) : 
+            (
+                (questionNumber < questionnaire.length) && (
+                    <>    
+                    <p>Reponse Selectionnee : {reponseSelect} | {questionNumber+1}/{questionnaire.length}</p>
+                    <Question question={questionnaire[questionNumber].question}/>
+                    {
+                        questionnaire[questionNumber].choices.map(
+                            (elem, index) => <Reponse value={elem} onReponseSelect={setReponseSelect} index={index} key={index} selected={reponseSelect}/>
+                        )
+                    }
+                    <button onClick={() => nextQuestion()}>Next</button>
+                    </>
+                )
             )}
         </div>
     )
