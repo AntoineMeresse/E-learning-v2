f46abedc3de172b2cc6996b9eec70b2439238b15
Antoine Meresse <ant.meresse@gmail.com>
2020-12-10 17:53:12 +0100

    chore: init quizz component

---
+++ src/components/Quizz.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Quizz() {
+    return (
+        <div className="quizz">
+            <h1>I'm a quizz component</h1>
+        </div>
+    )
+}
+export default Quizz
