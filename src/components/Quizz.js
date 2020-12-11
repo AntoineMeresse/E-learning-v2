4f4f728401ad123396f1038e35a4b0406d48bac1
Antoine Meresse <ant.meresse@gmail.com>
2020-12-11 12:28:37 +0100

    feat : quizz time working

--- src/components/Quizz.js
+++ src/components/Quizz.js
@@ -4,11 +4,17 @@ import Reponse from './Reponse'
 
 function Quizz({questionnaire}) {
     
+    const currentDate = new Date();
+
     const [reponseSelect , setReponseSelect] = useState(-1);
     const [questionNumber, setQuestionNumber] = useState(-1);
     const [compteur, setCompteur] = useState(0);
     const [timers, setTimers] = useState([]);
-
+    const [previousTime, setPreviousTime] = useState();
+    
+    /**
+     * Function to select the next Question
+     */
     function nextQuestion() {
         if (reponseSelect !== -1) {
             if(reponseSelect === questionnaire[questionNumber].correctAnswer) {
@@ -17,19 +23,39 @@ function Quizz({questionnaire}) {
             setQuestionNumber(questionNumber+1);
             setReponseSelect(-1);
             
+            console.log("Question n° : "+questionNumber)
             addTimers()
         }
     }
 
+    /**
+     * Function to start the quizz
+     */
     function startQuizz() {
-        console.log("Debut");
-        addTimers()
+        addTimers();
         setQuestionNumber(0);
     }
 
+    /**
+     * Function to calculate seconds between 2 dates 
+     */
+    function diffSeconds(d1,d2) {
+        return Math.abs((d2-d1) / 1000);
+    }
+
+    /**
+     * Function to add to timers list the time spent to answer a question
+     */
     function addTimers(){
-        setTimers([...timers, new Date()])
-        console.log(timers);
+        let currentTime = currentDate.getTime()
+        console.log("Current time vaut : "+currentTime);
+        if(timers.length === 0) {
+            setTimers(timers => [...timers, 0 ]) // Start : 0s
+        }
+        else {
+            setTimers(timers => [...timers, diffSeconds(previousTime, currentTime)]) // Diff previous and actualtime in second
+        }
+        setPreviousTime(currentTime)
     }
     
     return (
@@ -53,7 +79,9 @@ function Quizz({questionnaire}) {
                     </>
                 )
             )}
+            <p>Timers : {timers.toString()}</p>
         </div>
     )
 }
+
 export default Quizz
