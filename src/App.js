32335435dd25e15af1ab1da2546c569c8b217c71
Antoine Meresse <ant.meresse@gmail.com>
2020-12-11 15:38:12 +0100

    chore: fix import & import signup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Signup from './components/authentification/Signup';
 import Quizz from './components/Quizz';
 import questionnaire from './datas/questionnaire.json'
 
 function App() {
   return (
     <div className="App">
+      {/*
       <h1>E-learning</h1>
       <Quizz questionnaire={questionnaire}/>
+      */}
+      <Signup/>
     </div>
   );
 }
