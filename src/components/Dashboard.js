00735f3d6606f4f87d262d3f458a0f6f29988214
Antoine Meresse <ant.meresse@gmail.com>
2020-12-11 18:38:07 +0100

    feat: logout function

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Quizz from './Quizz';
 import questionnaire from '../datas/questionnaire.json'
 import { useAuth } from '../contexts/AuthContext'
+import { useHistory } from 'react-router-dom'
 
 function Dashboard() {
     
-    const { currentUser } = useAuth();
-    
+    const [error , setError] = useState('');
+    const { currentUser, logout } = useAuth();
+    const history = useHistory();
+
+    async function handleLogout() {
+        setError('');
+        try {
+            await logout();
+            history.push('/login');
+        }
+        catch {
+            setError('Failed to logout');
+        }
+
+    }
+
     return (
         <div className="dashboard">
             <div className="accountInfo">
                 <p>Current User Email : {currentUser.email}</p>
-                <button>Log out</button>
+                <button onClick={handleLogout}>Log out</button>
             </div>
             <br></br>
             <Quizz questionnaire={questionnaire}/>
