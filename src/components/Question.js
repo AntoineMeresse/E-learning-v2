363fb031c97a322395ec29b52595a4b608cec27f
Antoine Meresse <ant.meresse@gmail.com>
2020-12-11 18:14:59 +0100

    chore: change <p> to <h3>

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Question({question}) {
     return (
         <div className="question">
-            <p>Question : {question} </p>
+            <h3>Question : {question} </h3>
         </div>
     )
 }
