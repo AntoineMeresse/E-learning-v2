c93d937d6fbedfebd4ec486d3e479b87111f8754
Antoine Meresse <ant.meresse@gmail.com>
2020-12-16 14:20:40 +0100

    chore: delete user infos for now

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,34 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Quizz from './Quizz';
 import questionnaire from '../datas/questionnaire.json'
-import { useAuth } from '../contexts/AuthContext'
-import { useHistory } from 'react-router-dom'
+//import { useAuth } from '../contexts/AuthContext'
+//import { useHistory } from 'react-router-dom'
 
 function Dashboard() {
     
-    const [error , setError] = useState('');
-    const { currentUser, logout } = useAuth();
-    const history = useHistory();
-
-    async function handleLogout() {
-        setError('');
-        try {
-            await logout();
-            history.push('/login');
-        }
-        catch {
-            setError('Failed to logout');
-        }
-
-    }
+    //const [error , setError] = useState('');
+    //const { currentUser } = useAuth();
+    //const history = useHistory();
 
     return (
         <div className="dashboard">
-            <div className="accountInfo">
-                <p>Current User Email : {currentUser.email}</p>
-                <button onClick={handleLogout}>Log out</button>
-            </div>
-            <br></br>
             <Quizz questionnaire={questionnaire}/>
         </div>
     )
