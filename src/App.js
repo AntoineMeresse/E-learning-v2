fb99941c5df7f95c501afe796dfe81236b92c44a
Antoine Meresse <ant.meresse@gmail.com>
2020-12-16 14:21:39 +0100

    chore: add navbar

--- src/App.js
+++ src/App.js
@@ -6,22 +6,24 @@ import { AuthProvider } from './contexts/AuthContext';
 import { BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 import Dashboard from './components/Dashboard';
 import PrivateRoute from './components/PrivateRoute'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center" style={{minHeight : "100vh"}}>
-      <div className="w-100" style={{maxWidth: '800px'}}>
-        <Router>
-          <AuthProvider>
-            <Switch>
-              <PrivateRoute exact path="/" component={Dashboard}/>
-              <Route path="/signup" component={Signup}/>
-              <Route path="/login" component={Login}/>
-            </Switch>
-          </AuthProvider>
-        </Router>
-      </div>  
-    </Container>
+    <AuthProvider>
+      <Router>
+        <Navbar/>
+        <Container className="d-flex align-items-center justify-content-center" style={{minHeight : "90vh"}}>
+          <div className="w-100" style={{maxWidth: '800px'}}>
+                <Switch>
+                  <PrivateRoute exact path="/" component={Dashboard}/>
+                  <Route path="/signup" component={Signup}/>
+                  <Route path="/login" component={Login}/>
+                </Switch>
+          </div>  
+        </Container>
+      </Router>
+    </AuthProvider>
   );
 }
 
