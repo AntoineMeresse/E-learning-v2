c24e16e9607390349ab2e1e21b8d63317d5fc681
Antoine Meresse <ant.meresse@gmail.com>
2020-12-17 20:45:50 +0100

    feat: graph working for each user (only quizz1)

--- src/components/AdminDashBoard.js
+++ src/components/AdminDashBoard.js
@@ -6,45 +6,78 @@ import { useAuth } from '../contexts/AuthContext';
 function AdminDashBoard() {
 
     const [quizzId, setQuizzId] = useState('quizz1');
+    
     const [userId, setUserId] = useState('');
     const [datas, setDatas] = useState({});
 
     const { getQuizzRes } = useAuth();
+    const colors = ['red', 'blue', 'green', 'yellow'];
 
     async function fetchDataQuizz() {
-        try {
-            const doc = await getQuizzRes(quizzId);
-            if (!doc.exists) console.log(`Document (${quizzId}) doesn't exist`);
-            else {
-                const datas = doc.data();
-                console.log(`Document (${quizzId}) data:`, datas);
-                setDatas(datas);
-            }
-        }
-        catch {
-            console.log("Error when getting res");
+        const doc = await getQuizzRes(quizzId);
+        if (!doc.exists) console.log(`Document (${quizzId}) doesn't exist`);
+        else {
+            const d = doc.data();
+            console.log(`Document (${quizzId}) data:`, d);
+            setDatas(d);
         }
     }
 
     useEffect(() => {
-		fetchDataQuizz();
-	}, [])
-     
+        fetchDataQuizz();
+    }, [userId])
+
+    function handleChange(event) {
+        setUserId(event.target.value);
+    }
+
+    function createListOfLength(value){
+        let res = []
+        for(let i = 0; i<value; i++) res.push(i+1);
+        return res;
+    }
+
+    function createDatasets(){
+        let res = [];
+        let index = 0;
+        if (userId !== '') {
+            for (const [key, value] of Object.entries(datas.res[userId])) {
+                console.log(`${key}: ${value} (index : ${index})`);
+                let tmp = {
+                    label : value.dateString,
+                    data : value.time.slice(1),
+                    backgroundColor : colors[index],
+                };
+                index++;
+                res.push(tmp);
+            }
+        }
+        return res;
+    }
+
+
     return (
         <div className="admin-dashboard">
+            <p>Users list : </p>
             <p>Quizz id : {quizzId}</p>
             <p>User id : {userId}</p>
+            <p>{datas.questionNumber}</p>
+            
+            <select onChange={handleChange}>   
+                <option value=''>Select an user</option>
+                {
+                    datas.res && Object.entries(datas.res).map((elem, index) => <option value={elem[0]} key={index}>{elem[0]})</option>)
+                }
+            </select>
+
+            {
+                userId !== '' && console.log("Test : ", datas.res[userId])
+            }
 
             <Bar
                 data = {{
-                    labels : ['1', '2', '3'],
-                    datasets: [
-                        {
-                            label : 'resultats',
-                            data : [12,2,8],
-                            backgroundColor : 'blue',
-                        }
-                    ]
+                    labels : createListOfLength(datas.questionNumber),
+                    datasets : createDatasets(),
                 }} 
             />
         </div>
@@ -52,3 +85,14 @@ function AdminDashBoard() {
 }
 
 export default AdminDashBoard
+
+/*
+
+datasets: [
+    {
+        label : 'resultats',
+        data : [12,2,8],
+        backgroundColor : 'blue',
+    }
+]
+*/
