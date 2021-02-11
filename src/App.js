d7bdd1e03758a8e6cb7b968b768ddc279d4d6995
Antoine Meresse <ant.meresse@gmail.com>
2021-02-11 12:25:54 +0100

    chore: remove useless import

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
-import Signup from './components/authentification/Signup';
-import Login from './components/authentification/Login';
+
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from './contexts/AuthContext';
 import { BrowserRouter as Router,Switch,Route} from 'react-router-dom'
-import Dashboard from './components/Dashboard';
 import PrivateRoute from './components/PrivateRoute'
 import Navbar from './components/Navbar';
 import React, {useState} from 'react'
+
+// Components
 import AdminDashBoard from './components/admin/AdminDashBoard';
-import ManageQuizz from './components/admin/admin-quizz/ManageQuizz'
+import Dashboard from './components/Dashboard';
+import Signup from './components/authentification/Signup';
+import Login from './components/authentification/Login';
 
 function App() {
   
