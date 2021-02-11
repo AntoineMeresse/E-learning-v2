e9237c0b01e2b07ccbc6430d4abadfa7276159c2
Antoine Meresse <ant.meresse@gmail.com>
2021-02-11 17:57:09 +0100

    chore: redirect when add quizz

--- src/components/admin/admin-quizz/CreateQuizz.jsx
+++ src/components/admin/admin-quizz/CreateQuizz.jsx
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from 'react'
-import { Button } from 'react-bootstrap';
+import { useHistory } from "react-router-dom";
 
+import { Button } from 'react-bootstrap';
 import { useAuth } from '../../../contexts/AuthContext';
 
 function CreateQuizz() {
 
+    let history = useHistory();
     const { saveNewQuizz } = useAuth();
     
     const [name, setName] = useState("");
@@ -85,10 +87,11 @@ function CreateQuizz() {
         return res;
     }
 
-    function saveQuizz(){
+    function saveQuizz() {
         let datas = toJson();
         if (datas.length >= 0 && name.length >= 0) {
             saveNewQuizz(name, datas);
+            history.push("/");
         }
     }
 
